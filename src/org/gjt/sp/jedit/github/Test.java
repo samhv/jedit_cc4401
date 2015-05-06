ae2d3c4a7b77081824c636871a440c0959a4b8f2
Pablo Bustamante <ppbb15@gmail.com>
2015-05-06 16:22:42 -0300

    Se modificó clase de la interface de GitHub

---
+++ src/org/gjt/sp/jedit/github/Test.java
@@ -0,0 +1,11 @@
+package org.gjt.sp.jedit.github;
+
+public class Test {
+
+	public static void main(String[] args) {
+			Ventana v = new Ventana();
+		
+
+	}
+
+}
--- src/org/gjt/sp/jedit/github/Ventana.java
+++ src/org/gjt/sp/jedit/github/Ventana.java
@@ -1,19 +1,28 @@
 package org.gjt.sp.jedit.github;
 
+
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
-public class Ventana extends JFrame implements ActionListener {
+
+public class Ventana extends JFrame {
 
 	private Container contenedor;
-	
+	private JPanel panel_login;
+	private JPanel panel_commit;
+	private JPanel panel_push;
+	private JPanel panel_issues;
+	private JPanel panel_clonar;
 
 	public Ventana() {
 		/** permite iniciar las propiedades de los componentes */
@@ -21,8 +30,8 @@ public class Ventana extends JFrame implements ActionListener {
 		/** Asigna un titulo a la barra de titulo */
 		setTitle("GitHub");
 		/** tamaño de la ventana */
-		setSize(300, 180);
-		
+		setSize(400, 180);
+
 		/** pone la ventana en el Centro de la pantalla */
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,44 +43,137 @@ public class Ventana extends JFrame implements ActionListener {
 		contenedor.setLayout(new GridLayout(1, 1));
 		/** instanciamos el contenedor */
 		/**
-	
-		
-		/** Agregamos los componentes al Contenedor */
-	
+		 * /** Agregamos los componentes al Contenedor
+		 */
+
 		JTabbedPane panel_tab = new JTabbedPane();
-        contenedor.add(panel_tab);
-        JPanel panel_login = new JPanel();
-        JPanel panel_commit = new JPanel();
-        JPanel panel_push = new JPanel();
-        JPanel panel_issues = new JPanel();
+		contenedor.add(panel_tab);
+		// JPanel panel_login = new JPanel();
+		// JPanel panel_commit = new JPanel();
+		// JPanel panel_push = new JPanel();
+		// JPanel panel_issues = new JPanel();
 
-        /** Labels para cada panel */
-        
-        JLabel label_panel_login = new JLabel();
-        label_panel_login.setText("Panel para realizar el login");
-        JLabel label_panel_commit = new JLabel();
-        label_panel_commit.setText("Panel para realizar commit");
-        JLabel label_panel_push = new JLabel();
-        label_panel_push.setText("Panel para realizar los push");
-        JLabel label_panel_issues = new JLabel();
-        label_panel_issues.setText("Panel para realizar los issues");
-        
-        panel_login.add(label_panel_login);
-        panel_commit.add(label_panel_commit);
-        panel_push.add(label_panel_push);
-        panel_issues.add(label_panel_issues);
-      
-        panel_tab.addTab("Login", panel_login);
-        panel_tab.addTab("Commit", panel_commit);
-        panel_tab.addTab("Push", panel_push);
-        panel_tab.addTab("Issues", panel_issues);
-        
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		createLoginPanel();
+		createCommitPanel();
+		createPushPanel();
+		createClonarPanel();
+		panel_issues = new JPanel();
+		panel_tab.addTab("Login", panel_login);
+		panel_tab.addTab("Clone", panel_clonar);
+		panel_tab.addTab("Commit", panel_commit);
+		panel_tab.addTab("Push", panel_push);
+		panel_tab.addTab("Issues", panel_issues);
 
 	}
+
+	public void createLoginPanel() {
+
+		// Create and populate the panel.
+		String[] labels = { "Name: ", "Password: " };
+		int numPairs = labels.length;
+
+		panel_login = new JPanel(new SpringLayout());
+		for (int i = 0; i < numPairs; i++) {
+			JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+			panel_login.add(l);
+			JTextField textField = new JTextField(10);
+			l.setLabelFor(textField);
+			panel_login.add(textField);
+		}
+		JButton boton_conectar = new JButton("Connect");
+
+		JButton boton_cancelar = new JButton("Cancel");
+		boton_cancelar.addActionListener(new ListenerClose(this));
+		panel_login.add(boton_cancelar);
+		panel_login.add(boton_conectar);
+		// Lay out the panel.
+		SpringUtilities.makeCompactGrid(panel_login, numPairs + 1, 2, // rows,
+																		// cols
+				6, 6, // initX, initY
+				6, 6); // xPad, yPad
+
+	}
+	public void createCommitPanel() {
+		// Create and populate the panel.
+				String[] labels = { "Comment: " };
+				int numPairs = labels.length;
+
+				panel_commit = new JPanel(new SpringLayout());
+				for (int i = 0; i < numPairs; i++) {
+					JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+					panel_commit.add(l);
+					JTextField textField = new JTextField(10);
+					l.setLabelFor(textField);
+					panel_commit.add(textField);
+				}
+				JButton boton_commit = new JButton("Commit");
+
+				JButton boton_cancelar = new JButton("Cancel");
+				boton_cancelar.addActionListener(new ListenerClose(this));
+				panel_commit.add(boton_cancelar);
+				panel_commit.add(boton_commit);
+				// Lay out the panel.
+				SpringUtilities.makeCompactGrid(panel_commit, numPairs + 1, 2, // rows,
+																				// cols
+						6, 6, // initX, initY
+						6, 6); // xPad, yPad
+	}
+	
+	public void createPushPanel(){
+		panel_push = new JPanel();
+		JButton boton_push = new JButton("Push");
+		panel_push.add(boton_push);
+		
+//		// Lay out the panel.
+//		SpringUtilities.makeCompactGrid(panel_push,
+//				1, 1, // rows,// cols
+//				6, 6, // initX, initY
+//				6, 6); // xPad, yPad
+	}
+	public void createClonarPanel(){
+		panel_clonar = new JPanel();
+		// Create and populate the panel.
+				String[] labels = { "Url: ", "Local path: " };
+				int numPairs = labels.length;
+
+				panel_clonar = new JPanel(new SpringLayout());
+				for (int i = 0; i < numPairs; i++) {
+					JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+					panel_clonar.add(l);
+					JTextField textField = new JTextField(10);
+					l.setLabelFor(textField);
+					panel_clonar.add(textField);
+				}
+				JButton boton_conectar = new JButton("Clone");
+
+				JButton boton_cancelar = new JButton("Cancel");
+				boton_cancelar.addActionListener(new ListenerClose(this));
+				panel_clonar.add(boton_cancelar);
+				panel_clonar.add(boton_conectar);
+				// Lay out the panel.
+				SpringUtilities.makeCompactGrid(panel_clonar, numPairs + 1, 2, // rows,
+																				// cols
+						6, 6, // initX, initY
+						6, 6); // xPad, yPad
+
+		
+	}
+	
+	
+	
+	public class ListenerClose implements ActionListener{
+		private JFrame frame;
+		
+		public ListenerClose(JFrame frame){
+			this.frame = frame;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.dispose();
+			
+		}
+		
+	}
+	
 
 }
